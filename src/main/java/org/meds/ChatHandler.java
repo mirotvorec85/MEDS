633a298d3ec22936cfd04f5c98dcae8ee05845b6
Romman <roman.savoskin@gmail.com>
2015-01-06 00:18:32 +0200

    Added chat command for Parameter values debugging.

--- src/main/java/org/meds/ChatHandler.java
+++ src/main/java/org/meds/ChatHandler.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.meds.enums.Parameters;
 import org.meds.logging.Logging;
 import org.meds.map.Location;
 import org.meds.map.Map;
@@ -32,6 +33,7 @@ public final class ChatHandler
         chatCommands.put("tlffa", new TeamLootChatCommand(Group.TeamLootModes.Regular));
         chatCommands.put("tlrandom", new TeamLootChatCommand(Group.TeamLootModes.Random));
         chatCommands.put("tlleader", new TeamLootChatCommand(Group.TeamLootModes.Leader));
+        chatCommands.put("parameters", new ParametersChatCommand());
     }
 
     public static void sendSystemMessage(Player player, String message)
@@ -228,6 +230,48 @@ public final class ChatHandler
             if (player.getSession() != null) {
                 player.getSession().sendServerMessage(group.getTeamLootMode().getModeMessage())
                         .send(group.getTeamLootData());
+            }
+        }
+    }
+
+    private static class ParametersChatCommand extends ChatCommand {
+
+        @Override
+        public void handle(Player player, String[] args) {
+            int index = 0;
+            if (args.length > 0) {
+                index = SafeConvert.toInt32(args[0], 4);
+            }
+
+            UnitParameters.Parameter parameter = null;
+            switch (index) {
+                case 0:
+                    parameter = player.getParameters().base();
+                    sendSystemMessage(player, "Base Parameters:");
+                    break;
+                case 1:
+                    parameter = player.getParameters().guild();
+                    sendSystemMessage(player, "Guild Parameters:");
+                    break;
+                case 2:
+                    parameter = player.getParameters().equipment();
+                    sendSystemMessage(player, "Equipment Parameters:");
+                    break;
+                case 3:
+                    parameter = player.getParameters().magic();
+                    sendSystemMessage(player, "Magic Parameters:");
+                    break;
+            }
+
+            if (parameter == null) {
+                sendSystemMessage(player, "Total Parameters:");
+                for (Parameters value : Parameters.values()) {
+                    sendSystemMessage(player, value.name() + " = " + player.getParameters().value(value));
+                }
+            } else {
+                for (Parameters value : Parameters.values()) {
+                    sendSystemMessage(player, value.name() + " = " + parameter.value(value));
+                }
             }
         }
     }
